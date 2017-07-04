03fe1118b2c0c8df473daa406952939b933d8611
AliakseiBabitski <Aliaksei.Babitski@effective-soft.com>
2017-07-04 17:05:28 +0300

    Small Refactoring: extract duplicate code to method

--- src/main/java/com/ubs/opsit/interviews/base/BerlinClockConverter.java
+++ src/main/java/com/ubs/opsit/interviews/base/BerlinClockConverter.java
@@ -2,6 +2,8 @@ package com.ubs.opsit.interviews.base;
 
 import com.ubs.opsit.interviews.TimeConverter;
 
+import java.util.stream.Stream;
+
 /**
  * Created by Pototsky Ivan on 7/3/17.
  */
@@ -11,51 +13,44 @@ public class BerlinClockConverter implements TimeConverter {
     public String convertTime(String aTime) {
         StringBuilder builder = new StringBuilder();
 
-        String[] array = aTime.split(TIME_SEPARATOR);
+        int[] array = Stream.of(aTime.split(TIME_SEPARATOR)).mapToInt(s -> Integer.parseInt(s)).toArray();
         yellowLamp(builder, array[2]);
-        Integer hours = Integer.parseInt(array[0]);
-        Integer minutes = Integer.parseInt(array[1]);
-        fistHoursRow(builder, hours);
-        secondHoursRow(builder, hours);
-        firstMinutesRow(builder, minutes);
-        secondMinutesRow(builder, minutes);
+        fistHoursRow(builder, array[0]);
+        secondHoursRow(builder, array[0]);
+        firstMinutesRow(builder, array[1]);
+        secondMinutesRow(builder, array[1]);
         return builder.toString();
     }
 
-    private void yellowLamp(StringBuilder builder, String aSeconds) {
-        Integer seconds = Integer.parseInt(aSeconds);
+    private void yellowLamp(StringBuilder builder, int seconds) {
         builder.append(seconds % 2 == 0 ? "Y" : "O").append(System.lineSeparator());
     }
 
-    private void fistHoursRow(StringBuilder builder, Integer hours) {
-        int numberOfLamps = hours / 5;
-        for (int i = 0; i < 4; i++) {
-            builder.append(i < numberOfLamps ? "R" : "O");
-        }
-        builder.append(System.lineSeparator());
+    private void fistHoursRow(StringBuilder builder, int hours) {
+        initDefaultRow(builder, hours / 5, "R", false);
     }
 
-    private void secondHoursRow(StringBuilder builder, Integer hours) {
-        int numberOfLamps = hours % 5;
-        for (int i = 0; i < 4; i++) {
-            builder.append(i < numberOfLamps ? "R" : "O");
-        }
-        builder.append(System.lineSeparator());
+    private void secondHoursRow(StringBuilder builder, int hours) {
+        initDefaultRow(builder, hours % 5, "R", false);
     }
 
-    private void firstMinutesRow(StringBuilder builder, Integer minutes) {
+    private void firstMinutesRow(StringBuilder builder, int minutes) {
         int numberOfLamps = minutes / 5;
         for (int i = 0; i < 11; i++) {
-            builder.append(i < numberOfLamps ? i % 3 == 2 ? "R" : "Y"
+            builder.append(i < numberOfLamps ? (i % 3 == 2 ? "R" : "Y")
                     : "O");
         }
         builder.append(System.lineSeparator());
     }
 
-    private void secondMinutesRow(StringBuilder builder, Integer minutes) {
-        int numberOfLamps = minutes % 5;
+    private void secondMinutesRow(StringBuilder builder, int minutes) {
+        initDefaultRow(builder, minutes % 5, "Y", true);
+    }
+
+    private void initDefaultRow(StringBuilder builder, int switchedOnLamps, String on, boolean lastRow) {
         for (int i = 0; i < 4; i++) {
-            builder.append(i < numberOfLamps ? "Y" : "O");
+            builder.append(i < switchedOnLamps ? on : "O");
         }
+        if (!lastRow) builder.append(System.lineSeparator());
     }
 }
--- src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
@@ -1,7 +1,6 @@
 package com.ubs.opsit.interviews;
 
 import com.ubs.opsit.interviews.base.BerlinClockConverter;
-import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.junit.Test;
